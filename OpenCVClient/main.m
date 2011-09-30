7332d8743729225f6fc4babb5b05f0461c2246c1
aptogo <robin@aptogo.co.uk>
2011-09-30 07:50:30 -0400

    Updated copyright messages

--- OpenCVClient/main.m
+++ OpenCVClient/main.m
@@ -5,6 +5,12 @@
 //  Created by Robin Summerhill on 02/09/2011.
 //  Copyright 2011 Aptogo Limited. All rights reserved.
 //
+//  Permission is given to use this source code file without charge in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
 
 #import <UIKit/UIKit.h>
 
